32754f146546252f0ff9f7be2659653faaffaf7b
GabrielJuniorDev <110058841+Gabrli@users.noreply.github.com>
2023-06-26 09:54:28 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,23 @@
-const searchInputElement = document.querySelector("#input-search");
-const searchBtn = document.querySelector("#btn-serach");
-const Content = document.querySelector(".main-content-box");
-const tempCity = document.querySelector("#temp-city");
-const weatherCitInfo = document.querySelector("#weather-city-info");
-const Modal = document.querySelector(".info-container");
-const windText = document.querySelector("#wind");
-const humText = document.querySelector("#hum");
-const imgInfo = document.querySelector(".img-info");
+import { tempCity } from "./setAppLayout.js";
+import { weatherCitInfo } from "./setAppLayout.js";
+import { windText } from "./setAppLayout.js";
+import { humText } from "./setAppLayout.js";
+import { imgInfo } from "./setAppLayout.js";
+import { labelContentBox } from "./setAppLayout.js";
+import { apiKey } from "./dataJsonInfo.js";
+import { apiUrl } from "./dataJsonInfo.js";
+import { searchInputElement } from "./setAppLayout.js";
+import { searchBtn } from "./setAppLayout.js";
+import { Content } from "./setAppLayout.js";
 
-const apiKey = "be5d2ef6c0eacdccf7009d7a29f6e3e9";
 let inputSearchValue = document.querySelector("#input-search").value;
 
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=`;
-
-window.onload = () => {
-  weatherCitInfo.classList.add("hidden-elements");
-  tempCity.classList.add("hidden-elements");
-  windText.classList.add("hidden-elements");
-  humText.classList.add("hidden-elements");
-  imgInfo.classList.add("hidden-elements");
-};
 async function checkDataWheater(city) {
   if (city === "") {
     searchInputElement.classList.add("search-error");
   } else {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
-
     ActiveElement();
 
     tempCity.innerHTML =
@@ -59,6 +50,7 @@ const ActiveElement = () => {
     imgInfo.classList.remove("hidden-elements");
   }, "200");
   Content.classList.add("active-content");
+  labelContentBox.classList.remove("zoom-label-onload");
 };
 
 const TextHandler = (event) => {
